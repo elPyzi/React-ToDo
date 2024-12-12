3a7426f6d36b718eff117803c12dddac03cf8d01
Danila <danila.pyzhikov@gmail.com>
2024-12-12 15:02:56 +0300

    feat: user can add tasks and add to tasks titile

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,16 +1,49 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import '../assets/styles/ToDo.scss';
 
 function ToDo() {
     const [task, setTask] = useState('');
     const [time, setTime] = useState('00:00');
-    const [done, setDone] = useState(false);
+    const [toDoList, setToDoList] = useState([]);
+    const [color, setColor] = useState('item1');
+    const [isActiveSelect, setActiveSelect] = useState(false);
+
+    useEffect(() => {
+        if (toDoList.length === 0) {
+            const defaultTask = {
+                id: 1,
+                task: 'Купить яблоки',
+                time: '8:00',
+                taskColor: 'item1',
+            };
+            setToDoList([defaultTask]);
+        }
+    }, []);
+
+    const cleanInputTask = () => {
+        setTime('00:00');
+        setTask('');
+    };
 
     const addTask = () => {
-        // const newTask = {
-        //     task: task,
-        //     time: time,
-        // };
+        const isEmpty = task == '' ? false : true;
+        if (isEmpty) {
+            const newTask = {
+                id: toDoList.length + 1,
+                task: task,
+                time: time[0].startsWith('0')
+                    ? time.replace('0', '')
+                    : time.trim(),
+                taskColor: color,
+            };
+            setToDoList([...toDoList, newTask]);
+            cleanInputTask();
+        }
+    };
+
+    const removeTask = (taskId) => {
+        const updatedTasks = toDoList.filter((tasks) => taskId !== tasks.id);
+        setToDoList(updatedTasks);
     };
 
     return (
@@ -18,48 +51,90 @@ function ToDo() {
             <h1 className="to-do__heading">Today main focus</h1>
             <h2 className="to-do__slogan">Do your day better</h2>
             <div className="to-do__tasks-container">
-                <div className="to-do__input-container">
-                    <div className="to-do__filter">
-                        <div className="to-do__filter-item"></div>
-                        <div className="to-do__filter-item"></div>
-                        <div className="to-do__filter-item"></div>
-                    </div>
-                    <input
-                        type="text"
-                        placeholder="What is your next task?"
-                        className="to-do_input"
-                        maxLength={40}
-                        value={task}
-                        onChange={(event) => setTask(event.target.value)}
-                    />
-                    <div className="to-do__actions">
+                <div className="to-do__filter-input">
+                    <div className="to-do__input-container">
                         <input
-                            type="time"
-                            className="to-do__time"
-                            value={time}
-                            onChange={(event) => setTime(event.target.time)}
+                            type="text"
+                            placeholder="What is your next task?"
+                            className="to-do_input"
+                            maxLength={40}
+                            value={task}
+                            onChange={(event) => setTask(event.target.value)}
                         />
-                        <button
-                            type="button"
-                            className="to-do__add-task"
-                            onClick={addTask}
-                        >
-                            add
-                        </button>
+                        <div className="to-do__actions">
+                            <input
+                                type="time"
+                                className="to-do__time"
+                                value={time}
+                                onChange={(event) =>
+                                    setTime(event.target.value)
+                                }
+                                min={'00:00'}
+                                max={'23:59'}
+                            />
+                            <button
+                                type="button"
+                                className="to-do__add-task"
+                                onClick={addTask}
+                            >
+                                add
+                            </button>
+                        </div>
+                    </div>
+                    <div
+                        className="to-do__filter filter"
+                        onClick={() => setActiveSelect(!isActiveSelect)}
+                    >
+                        <div className="filter__btn">
+                            <div className="filter__item"></div>
+                            <div className="filter__item"></div>
+                            <div className="filter__item"></div>
+                        </div>
+                        {isActiveSelect && (
+                            <div className="filter__dropdown-content">
+                                <div
+                                    className="filter__dropdown-item"
+                                    onClick={() => setColor('item1')}
+                                ></div>
+                                <div
+                                    className="filter__dropdown-item"
+                                    onClick={() => setColor('item2')}
+                                ></div>
+                                <div
+                                    className="filter__dropdown-item"
+                                    onClick={() => setColor('item3')}
+                                ></div>
+                                <div
+                                    className="filter__dropdown-item"
+                                    onClick={() => setColor('item4')}
+                                ></div>
+                                <div
+                                    className="filter__dropdown-item"
+                                    onClick={() => setColor('item5')}
+                                ></div>
+                            </div>
+                        )}
                     </div>
                 </div>
                 <ul className="to-do__container-tasks">
-                    <li className="to-do__task">
-                        <div className="to-do__title"></div>
-                        <div className="to-do__task-text">{task}</div>
-                        <div className="to-do__task-time">{time}</div>
-                        <button
-                            type="button"
-                            className={`to-do__done ${done === true ? 'done' : ''}`}
-                        >
-                            done
-                        </button>
-                    </li>
+                    {toDoList.map((task) => (
+                        <li key={task.id} className="to-do__task">
+                            <div>
+                                <div
+                                    className={`to-do__title ${task.taskColor === '' ? 'item1' : task.taskColor}`}
+                                ></div>
+                            </div>
+                            <div className="to-do__task-text">{task.task}</div>
+                            <div className="to-do__task-time">{task.time}</div>
+                            <button
+                                type="button"
+                                className={`to-do__done`}
+                                onClick={() => removeTask(task.id)}
+                            >
+                                done
+                            </button>
+                        </li>
+                    ))}
                 </ul>
             </div>
         </div>
